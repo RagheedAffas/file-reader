682215ae6f8d80a365ac7d5ae76350d44bd5fa21
RagheedAffas <eng.ragheed@outlook.com>
2016-04-02 18:25:09 -0400

    applying final changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,28 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException{
 
-        String filename = "C:/Users/High Tech/Desktop/text.txt";
+       String filename = "C:/Users/High Tech/Desktop/text.txt";
 
 
         File file = new File(filename);
 
         Scanner scanner = new Scanner(file);
 
+        System.out.println(scanner.nextInt());
+
+        scanner.nextLine();
+
+        int count = 2;
+
         while (scanner.hasNext()) {
 
-            System.out.println(scanner.nextLine());
+            System.out.println(count + ": " + scanner.nextLine());
+
+            count++;
 
         }
+
+        scanner.close();
 
     }
 }
